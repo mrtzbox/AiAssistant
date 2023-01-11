57e7ff1a0a425feacc1bab8c70604e08a4072cf1
Brian <mrtzbox@gmail.com>
2023-01-10 21:40:56 -0700

    cccc

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ app.post('/', async (req, res) => {
     console.log(error);
     res.status(500).send({ error })
   }
-}
+
 
 app.listen(5000, () => {
   console.log('Server is running on port http://localhost:5000');
