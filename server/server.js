feaeb75dabccddb4081f6f7b2f46e222d62031fa
Brian <mrtzbox@gmail.com>
2023-01-10 21:41:02 -0700

    ccccccccc

--- server/server.js
+++ server/server.js
@@ -26,11 +26,11 @@ app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
-    const response = await openai.createCompletion({
-      model="text-davinci-003",
+    const response = await openai.createCompletion(
+            model="text-davinci-003",
       prompt="",
-      temperature=0.9,
-      max_tokens=4000,
+      temperature=1.0,
+      max_tokens=3000,
       top_p=1,
       frequency_penalty=0,
       presence_penalty=0.6,
